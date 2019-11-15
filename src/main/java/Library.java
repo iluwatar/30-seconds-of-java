332f003ead0430bb9eb26f8263800f3aff839882
grzesiek <grzesiekkedzior@gmail.com>
2019-11-15 21:35:17 +0100

    Simple refactoring.

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -453,9 +453,9 @@ public class Library {
   public static void bubbleSort(int[] arr) {
     var lastIndex = arr.length - 1;
 
-    for(var j = 0; j < lastIndex; j++) {
-      for(var i = 0; i < lastIndex - j; i++) {
-        if(arr[i] > arr[i + 1]) {
+    for (var j = 0; j < lastIndex; j++) {
+      for (var i = 0; i < lastIndex - j; i++) {
+        if (arr[i] > arr[i + 1]) {
           var tmp = arr[i];
           arr[i] = arr[i + 1];
           arr[i + 1] = tmp;
