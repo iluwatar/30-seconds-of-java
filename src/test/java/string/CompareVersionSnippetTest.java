9642607167df5a5b626f46aa85c3f44539fdfa08
Eiman Iranpour <eiman.iranpour@gmail.com>
2020-10-16 02:42:02 +0330

    Set CompareVersionSnippet indent to 2

--- src/test/java/string/CompareVersionSnippetTest.java
+++ src/test/java/string/CompareVersionSnippetTest.java
@@ -9,46 +9,46 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CompareVersionSnippetTest {
-    /**
-     * Tests for {@link CompareVersionSnippet#compareVersion(String, String)}.
-     */
-    @Test
-    public void testCompareVersion() {
-        assertEquals(0, CompareVersionSnippet.compareVersion("awesome-app-2.3.4-SNAPSHOT", "2.3.4"));
-        assertEquals(0, CompareVersionSnippet.compareVersion("2.3.4-SNAPSHOT", "2.3.4"));
-        assertEquals(1, CompareVersionSnippet.compareVersion("2.3.4.1-SNAPSHOT", "2.3.4"));
-        assertEquals(0, CompareVersionSnippet.compareVersion("2.03.4", "2.3.4"));
-        assertEquals(-1, CompareVersionSnippet.compareVersion("2.03.4", "2.3.4.1"));
-        assertEquals(-1, CompareVersionSnippet.compareVersion("2.3.4", "2.30.4"));
-        assertEquals(0, CompareVersionSnippet.compareVersion("2.3.0", "2.3"));
-        assertEquals(1, CompareVersionSnippet.compareVersion("2.3.3", "2.3"));
+  /**
+   * Tests for {@link CompareVersionSnippet#compareVersion(String, String)}.
+   */
+  @Test
+  public void testCompareVersion() {
+    assertEquals(0, CompareVersionSnippet.compareVersion("awesome-app-2.3.4-SNAPSHOT", "2.3.4"));
+    assertEquals(0, CompareVersionSnippet.compareVersion("2.3.4-SNAPSHOT", "2.3.4"));
+    assertEquals(1, CompareVersionSnippet.compareVersion("2.3.4.1-SNAPSHOT", "2.3.4"));
+    assertEquals(0, CompareVersionSnippet.compareVersion("2.03.4", "2.3.4"));
+    assertEquals(-1, CompareVersionSnippet.compareVersion("2.03.4", "2.3.4.1"));
+    assertEquals(-1, CompareVersionSnippet.compareVersion("2.3.4", "2.30.4"));
+    assertEquals(0, CompareVersionSnippet.compareVersion("2.3.0", "2.3"));
+    assertEquals(1, CompareVersionSnippet.compareVersion("2.3.3", "2.3"));
 
-        // Test for sorting versions
-        List<String> versions = Arrays.asList(
-                "2.3.4.1",
-                "2.3.5-beta2",
-                "2.3.4.RELEASE",
-                "awesome-app-2.2",
-                "2.3.2-SNAPSHOT",
-                "2.2.1"
-        );
+    // Test for sorting versions
+    List<String> versions = Arrays.asList(
+        "2.3.4.1",
+        "2.3.5-beta2",
+        "2.3.4.RELEASE",
+        "awesome-app-2.2",
+        "2.3.2-SNAPSHOT",
+        "2.2.1"
+    );
 
-        List<String> expectedSortedVersion = Arrays.asList(
-                "awesome-app-2.2",
-                "2.2.1",
-                "2.3.2-SNAPSHOT",
-                "2.3.4.RELEASE",
-                "2.3.4.1",
-                "2.3.5-beta2"
-        );
+    List<String> expectedSortedVersion = Arrays.asList(
+        "awesome-app-2.2",
+        "2.2.1",
+        "2.3.2-SNAPSHOT",
+        "2.3.4.RELEASE",
+        "2.3.4.1",
+        "2.3.5-beta2"
+    );
 
-        String oldestVersion = versions.stream().min(CompareVersionSnippet::compareVersion).get();
-        assertEquals("awesome-app-2.2", oldestVersion);
+    String oldestVersion = versions.stream().min(CompareVersionSnippet::compareVersion).get();
+    assertEquals("awesome-app-2.2", oldestVersion);
 
-        String newestVersion = versions.stream().max(CompareVersionSnippet::compareVersion).get();
-        assertEquals("2.3.5-beta2", newestVersion);
-        
-        assertArrayEquals(expectedSortedVersion.toArray(),
-                versions.stream().sorted(CompareVersionSnippet::compareVersion).toArray());
-    }
+    String newestVersion = versions.stream().max(CompareVersionSnippet::compareVersion).get();
+    assertEquals("2.3.5-beta2", newestVersion);
+
+    assertArrayEquals(expectedSortedVersion.toArray(),
+        versions.stream().sorted(CompareVersionSnippet::compareVersion).toArray());
+  }
 }
