9b83f89a13928271eb2f6f38340cfbdc263a64c8
Eiman Iranpour <eiman.iranpour@gmail.com>
2020-10-16 02:29:42 +0330

    Add compare version snippet

---
+++ src/main/java/string/CompareVersionSnippet.java
@@ -0,0 +1,35 @@
+package string;
+
+public class CompareVersionSnippet {
+
+    private final static String EXTRACT_VERSION_REGEX = ".*?((?<!\\w)\\d+([.-]\\d+)*).*";
+
+    /**
+     * Compares two version strings.
+     * Credits: https://stackoverflow.com/a/6702000/6645088 and https://stackoverflow.com/a/44592696/6645088
+     *
+     * @param v1 the first version string to compare
+     * @param v2 the second version string to compare
+     * @return the value {@code 0} if the two strings represent same versions;
+     * a value less than {@code 0} if {@code v1} is greater than {@code v2}; and
+     * a value greater than {@code 0} if {@code v2} is greater than {@code v1}
+     */
+    public static int compareVersion(String v1, String v2) {
+        var components1 = getVersionComponents(v1);
+        var components2 = getVersionComponents(v2);
+        int length = Math.max(components1.length, components2.length);
+        for (int i = 0; i < length; i++) {
+            Integer c1 = i < components1.length ? Integer.parseInt(components1[i]) : 0;
+            Integer c2 = i < components2.length ? Integer.parseInt(components2[i]) : 0;
+            int result = c1.compareTo(c2);
+            if (result != 0) {
+                return result;
+            }
+        }
+        return 0;
+    }
+
+    private static String[] getVersionComponents(String version) {
+        return version.replaceAll(EXTRACT_VERSION_REGEX, "$1").split("\\.");
+    }
+}
---
+++ src/test/java/string/CompareVersionSnippetTest.java
@@ -0,0 +1,54 @@
+package string;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CompareVersionSnippetTest {
+    /**
+     * Tests for {@link CompareVersionSnippet#compareVersion(String, String)}.
+     */
+    @Test
+    public void testCompareVersion() {
+        assertEquals(0, CompareVersionSnippet.compareVersion("awesome-app-2.3.4-SNAPSHOT", "2.3.4"));
+        assertEquals(0, CompareVersionSnippet.compareVersion("2.3.4-SNAPSHOT", "2.3.4"));
+        assertEquals(1, CompareVersionSnippet.compareVersion("2.3.4.1-SNAPSHOT", "2.3.4"));
+        assertEquals(0, CompareVersionSnippet.compareVersion("2.03.4", "2.3.4"));
+        assertEquals(-1, CompareVersionSnippet.compareVersion("2.03.4", "2.3.4.1"));
+        assertEquals(-1, CompareVersionSnippet.compareVersion("2.3.4", "2.30.4"));
+        assertEquals(0, CompareVersionSnippet.compareVersion("2.3.0", "2.3"));
+        assertEquals(1, CompareVersionSnippet.compareVersion("2.3.3", "2.3"));
+
+        // Test for sorting versions
+        List<String> versions = Arrays.asList(
+                "2.3.4.1",
+                "2.3.5-beta2",
+                "2.3.4.RELEASE",
+                "awesome-app-2.2",
+                "2.3.2-SNAPSHOT",
+                "2.2.1"
+        );
+
+        List<String> expectedSortedVersion = Arrays.asList(
+                "awesome-app-2.2",
+                "2.2.1",
+                "2.3.2-SNAPSHOT",
+                "2.3.4.RELEASE",
+                "2.3.4.1",
+                "2.3.5-beta2"
+        );
+
+        String oldestVersion = versions.stream().min(CompareVersionSnippet::compareVersion).get();
+        assertEquals("awesome-app-2.2", oldestVersion);
+
+        String newestVersion = versions.stream().max(CompareVersionSnippet::compareVersion).get();
+        assertEquals("2.3.5-beta2", newestVersion);
+        
+        assertArrayEquals(expectedSortedVersion.toArray(),
+                versions.stream().sorted(CompareVersionSnippet::compareVersion).toArray());
+    }
+}
