3e43c153e657f77923f84257f1cf8281b458c14d
byoungju94 <byoungju94@outlook.com>
2021-02-14 19:15:03 +0900

    add merge sort algorithm class and test code

---
+++ src/main/java/algorithm/MergeSortSnippet.java
@@ -0,0 +1,73 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2017-2019 Ilkka Seppälä
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package algorithm;
+
+/*
+ * 30 Seconds of Java code library
+ *
+ */
+public class MergeSortSnippet {
+
+  private static int[] tmpBuffer;
+
+  private static void recursiveMergeSortTask(int[] arr, int leftPointer, int rightPointer) {
+    if (leftPointer >= rightPointer)
+      return;
+
+    int arrPosition;
+    int centerPointer = (leftPointer + rightPointer) / 2;
+    int tmpRightPointer = 0;
+    int tmpLeftPointer = 0;
+    int mergePosition = leftPointer;
+
+    recursiveMergeSortTask(arr, leftPointer, centerPointer);
+    recursiveMergeSortTask(arr, centerPointer + 1, rightPointer);
+
+    for (arrPosition = leftPointer; arrPosition <= centerPointer; arrPosition++) {
+      tmpBuffer[tmpRightPointer++] = arr[arrPosition];
+    }
+
+    while (arrPosition <= rightPointer && tmpLeftPointer < tmpRightPointer)
+      arr[mergePosition++] = (tmpBuffer[tmpLeftPointer] <= arr[arrPosition])  
+                              ? tmpBuffer[tmpLeftPointer++]
+                              : arr[arrPosition++];
+
+    while (tmpLeftPointer < tmpRightPointer)
+      arr[mergePosition++] = tmpBuffer[tmpLeftPointer++];
+  }
+
+  /**
+   * Sort an array with mergeSort algorithm.
+   *
+   * @param arr array to sort
+   */
+  public static void mergeSort(int[] arr) {
+    tmpBuffer = new int[arr.length];
+
+    recursiveMergeSortTask(arr, 0, arr.length - 1);
+
+    tmpBuffer = null;
+  }
+}
