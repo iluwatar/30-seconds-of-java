4b9c614131c98a0aad213fd0f2b278bd5bbef367
Prakhar Sharma <44018122+prakharsharma27@users.noreply.github.com>
2021-10-10 05:30:33 +0530

    Update BinarySearchSnippet.java

--- src/main/java/algorithm/BinarySearchSnippet.java
+++ src/main/java/algorithm/BinarySearchSnippet.java
@@ -29,28 +29,27 @@ package algorithm;
  */
 import java.util.*;
 public class BinarySearchSnippet {
+    //
+    //     Sorted Array will be given and element will be found
+    //
     public static int binarySearch(int[] arr,int l,int r,int x){
-//        Arrays.sort(arr);
+
         if (r >= l) {
             int mid = l + (r - l) / 2;
 
-            // If the element is present at the
-            // middle itself
+          
             if (arr[mid] == x)
                 return mid;
 
-            // If element is smaller than mid, then
-            // it can only be present in left subarray
+           
             if (arr[mid] > x)
                 return binarySearch(arr, l, mid - 1, x);
 
-            // Else the element can only be present
-            // in right subarray
+           
             return binarySearch(arr, mid + 1, r, x);
         }
 
-        // We reach here when element is not present
-        // in array
+       
         return -1;
     }
 }
