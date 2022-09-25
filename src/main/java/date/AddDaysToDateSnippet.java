28cf7298a0576098c1b4dbd83911953ba131f30d
Ilkka Seppälä <ilkka.seppala@gofore.com>
2022-09-25 20:17:31 +0300

    Fix checkstyle errors

--- src/main/java/date/AddDaysToDateSnippet.java
+++ src/main/java/date/AddDaysToDateSnippet.java
@@ -5,18 +5,37 @@ import java.time.ZoneId;
 import java.util.Calendar;
 import java.util.Date;
 
+/**
+ * 30 Seconds of Java code library.
+ * AddDaysToDateSnippet
+ *
+ */
 public class AddDaysToDateSnippet {
-    public static Date addDaysToDate(Date date, int noOfDays){
-        if(date!=null){
-            Calendar cal = Calendar.getInstance();
-            cal.setTime(date);
-            cal.add(Calendar.DAY_OF_MONTH, noOfDays);
-            return cal.getTime();
-        }
-        return null;
+  /**
+   * Add days to given date.
+   *
+   * @param date given date
+   * @param noOfDays number of days to add
+   * @return modified date
+   */
+  public static Date addDaysToDate(Date date, int noOfDays) {
+    if (date != null) {
+      Calendar cal = Calendar.getInstance();
+      cal.setTime(date);
+      cal.add(Calendar.DAY_OF_MONTH, noOfDays);
+      return cal.getTime();
     }
+    return null;
+  }
 
-    public static LocalDate addDaysToLocalDate(LocalDate date, long noOfDays){
-        return date!=null ? date.plusDays(noOfDays) : null;
-    }
+  /**
+   * Add days to local date.
+   *
+   * @param date given local date
+   * @param noOfDays number of days to add
+   * @return modified date
+   */
+  public static LocalDate addDaysToLocalDate(LocalDate date, long noOfDays) {
+    return date != null ? date.plusDays(noOfDays) : null;
+  }
 }
