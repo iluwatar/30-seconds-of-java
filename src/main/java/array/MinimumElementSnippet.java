d2055b7532d3ead0dfc443ace92f36f3b0bd9368
Varada <mvarada2000@gmail.com>
2022-10-12 11:08:33 +0530

    Reformated minimum element snippet

--- src/main/java/array/MinimumElementSnippet.java
+++ src/main/java/array/MinimumElementSnippet.java
@@ -4,16 +4,16 @@ public class MinimumElementSnippet {
 
     /**
      * Returns minimum element of the array
+     *
      * @param array , the array of integers
      * @return minimum element of an array
      */
 
 
-
-    public static int findMin(int[] array){
+    public static int findMin(int[] array) {
         int min = array[0];
-        for(int i = 1; i<array.length;i++){
-            if(min>array[i]){
+        for (int i = 1; i < array.length; i++) {
+            if (min > array[i]) {
                 min = array[i];
             }
         }
--- src/test/java/array/MinimumElementSnippetTest.java
+++ src/test/java/array/MinimumElementSnippetTest.java
@@ -10,13 +10,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class MinimumElementSnippetTest {
 
-    MinimumElementSnippet array = new MinimumElementSnippet();
-
     @Test
-    void findMinTest(){
-        assertEquals(array.findMin(new int[]{5,11,3,6,8}),3);
-        assertEquals(array.findMin(new int[]{5}),5);
-        assertEquals(array.findMin(new int[]{0}),0);
+    void findMinTest() {
+        assertEquals(MinimumElementSnippet.findMin(new int[]{5, 11, 3, 6, 8}), 3);
+        assertEquals(MinimumElementSnippet.findMin(new int[]{5}), 5);
+        assertEquals(MinimumElementSnippet.findMin(new int[]{0}), 0);
     }
 
 }
