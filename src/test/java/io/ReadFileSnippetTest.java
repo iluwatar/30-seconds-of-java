ded87892e3c0f70e1303685507dd45ba259a7dbc
Ilkka Seppälä <ilkka.seppala@gofore.com>
2022-12-06 18:39:41 +0200

    add license header plugin

--- src/test/java/io/ReadFileSnippetTest.java
+++ src/test/java/io/ReadFileSnippetTest.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2017-2019 Ilkka Seppälä
+ * Copyright (c) 2017-2022 Ilkka Seppälä
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 
 package io;
 
