391742914827a5ebb72af6357cb537330c00ca33
Manish Dait <daitmanish88@gmail.com>
2023-01-26 23:35:38 +0530

    intendation fix

--- src/main/java/algorithm/MergeSortSnippet.java
+++ src/main/java/algorithm/MergeSortSnippet.java
@@ -28,57 +28,54 @@ package algorithm;
  * MergeSortSnippet.
  */
 
-public class MergeSortSnippet {
+public class MergeSortSnippet {  
+  /**
+     * Sort an array with qmergesort algorithm.
+     *
+     * @param arr   array to sort
+     *     left  left index where to begin sort (e.g. 0)
+     *     right index where to end sort (e.g. array length - 1)
+     */
 
-    /**
-   * Sort an array with qmergesort algorithm.
-   *
-   * @param arr   array to sort
-   * @param left  left index where to begin sort (e.g. 0)
-   * @param right right index where to end sort (e.g. array length - 1)
-   */
-
-public static void mergeSort(int arr[], int low, int high){
-    if(low>=high){
-        return;
+  public static void mergeSort(int[] arr, int low, int high) {
+    if (low >= high) {
+      return;
     }
-    var mid = (low+high)/2;
+    var mid = (low + high) / 2;
     mergeSort(arr, low, mid);
-    mergeSort(arr, mid+1, high);
+    mergeSort(arr, mid + 1, high);
     merge(arr, low, high, mid);
-}
+  }
 
-private static void merge(int[] arr, int low, int high, int mid) {
-    int temp[] = new int[(high-low+1)];
+  private static void merge(int[] arr, int low, int high, int mid) {
+    int[] temp = new int[(high - low + 1)];
     var i = low;
-    var j = mid+1;
+    var j = mid + 1;
     var k = 0;
 
-    while(i<=mid && j<=high){
-        if(arr[i]<arr[j]){
-            temp[k++] = arr[i];
-            i++;
-        }
-
-        else{
-            temp[k++] = arr[j];
-            j++;
-        }
-    }
-
-    while(i<=mid){
+    while (i <= mid && j <= high) {
+      if (arr[i] < arr[j]) {
         temp[k++] = arr[i];
         i++;
-    }
-
-    while(j<=high){
+      } else {
         temp[k++] = arr[j];
         j++;
+      }
     }
 
-    for(int m=0, n=low; m<temp.length; m++, n++){
-        arr[n] = temp[m];
+    while (i <= mid) {
+      temp[k++] = arr[i];
+      i++;
     }
-}
+
+    while (j <= high) {
+      temp[k++] = arr[j];
+      j++;
+    }
+
+    for (int m = 0, n = low; m < temp.length; m++, n++) {
+      arr[n] = temp[m];
+    }
+  }
     
 }
--- src/test/java/algorithm/MergeSortSnippetTest.java
+++ src/test/java/algorithm/MergeSortSnippetTest.java
@@ -22,31 +22,30 @@
  * SOFTWARE.
  */
 
- package algorithm;
+package algorithm;
 
  import static org.junit.jupiter.api.Assertions.assertEquals;
  
- import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
  
- 
- /*
+/*
   * Tests for 30 Seconds of Java code library
   *
   */
- class MergeSortSnippetTest {
-   /**
+class MergeSortSnippetTest {
+  /**
     * Tests for {@link MergeSortSnippet#mergeSort(int[], int, int)}.
     */
-   @Test
+  @Test
    void testMergeSort() {
-     var arr = new int[] {7, 13, 3, 1, 8, 5};
-     MergeSortSnippet.mergeSort(arr, 0, arr.length - 1);
-     assertEquals(6, arr.length);
-     assertEquals(1, arr[0]);
-     assertEquals(3, arr[1]);
-     assertEquals(5, arr[2]);
-     assertEquals(7, arr[3]);
-     assertEquals(8, arr[4]);
-     assertEquals(13, arr[5]);
-   }
- }
+    var arr = new int[] {7, 13, 3, 1, 8, 5};
+    MergeSortSnippet.mergeSort(arr, 0, arr.length - 1);
+    assertEquals(6, arr.length);
+    assertEquals(1, arr[0]);
+    assertEquals(3, arr[1]);
+    assertEquals(5, arr[2]);
+    assertEquals(7, arr[3]);
+    assertEquals(8, arr[4]);
+    assertEquals(13, arr[5]);
+  }
+}
