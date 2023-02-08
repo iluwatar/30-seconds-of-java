d64b2337f92bf7bd9a8a72b92bbb826c89e7479e
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2023-02-08 08:51:48 +0530

    remove IOException import unused

--- src/main/java/io/InputStreamToStringSnippet.java
+++ src/main/java/io/InputStreamToStringSnippet.java
@@ -25,7 +25,6 @@
 package io;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
