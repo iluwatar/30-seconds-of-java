fa9a60dd5c0d9e38897d58ff2ac38a847a41eb6c
Antonio Addeo <39912670+AddeusExMachina@users.noreply.github.com>
2023-03-25 18:36:29 +0100

    refactor: Optimize palindrome check to use O(1) space (#173)
    * Optimize isPalindrome method to use O(1) space Ref: #169
    
    * Update isPalindrome method in README.md files Ref: #169

--- src/main/java/string/PalindromCheckSnippet.java
+++ src/main/java/string/PalindromCheckSnippet.java
@@ -37,14 +37,19 @@ public class PalindromCheckSnippet {
    * @return true if palindrome
    */
   public static boolean isPalindrome(String s) {
-    var sb = new StringBuilder();
-    for (var c : s.toCharArray()) {
-      if (Character.isLetter(c)) {
-        sb.append(c);
+    for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+      while (i < j && !Character.isLetter(s.charAt(i))) {
+        i++;
+      }
+      while (i < j && !Character.isLetter(s.charAt(j))) {
+        j--;
+      }
+
+      if (Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))) {
+        return false;
       }
     }
-    var forward = sb.toString().toLowerCase();
-    var backward = sb.reverse().toString().toLowerCase();
-    return forward.equals(backward);
+
+    return true;
   }
 }
