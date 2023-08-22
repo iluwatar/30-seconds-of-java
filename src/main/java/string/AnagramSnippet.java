d2086ed1fcd7c2d630aa9a575298319f5d578265
Abdullah Khan <abeiy4u@gmail.com>
2023-08-22 13:03:52 +0530

    Update AnagramSnippet.java
    Moved the array declaration after the if statement to minimise the unnecessary array creation in case the function returns from the if statement.

--- src/main/java/string/AnagramSnippet.java
+++ src/main/java/string/AnagramSnippet.java
@@ -41,11 +41,14 @@ public class AnagramSnippet {
   public static boolean isAnagram(String s1, String s2) {
     var l1 = s1.length();
     var l2 = s2.length();
-    var arr1 = new int[256];
-    var arr2 = new int[256];
+
     if (l1 != l2) {
       return false;
     }
+
+    var arr1 = new int[256];
+    var arr2 = new int[256];
+    
     for (var i = 0; i < l1; i++) {
       arr1[s1.charAt(i)]++;
       arr2[s2.charAt(i)]++;
