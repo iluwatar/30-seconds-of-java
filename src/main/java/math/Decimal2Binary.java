71025f12759d3f35a9f62429ab324a4d4c5fd840
Srijit Chakraborty <121560359+srijitt@users.noreply.github.com>
2023-09-30 18:49:33 +0530

    Added 2 snippets
    BInary to Decimal and Decimal to Binary conversion snippets

---
+++ src/main/java/math/Decimal2Binary.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+/** Binary to Decimal Conversion
+ * Snippet
+ */
+public class Decimal2Binary {
+    /**
+     * reverse a string
+     * @param str string
+     * @return rev reversed string
+     */
+    public static String reverse(String str) {
+        String rev="";
+        for (int i = str.length()-1; i >= 0; i--) {
+            char ch = str.charAt(i);
+            rev += ch;
+        }
+        return rev;
+    }
+
+    /**
+     * converts decimal to binary
+     * @param n number
+     */
+    public static void decimalToBinary(int n) {
+        int d;
+        String dec="";
+        while(n!=0) {
+            d = n%2;
+            dec += Integer.toString(d);
+            n /= 2;
+        }
+
+        String binary = reverse(dec);
+        System.out.println(binary);
+    }
+
+
+    public static void main(String[] args) {
+        // user input
+        System.out.println("Enter a Number: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        
+        decimalToBinary(number);
+    }
+}
