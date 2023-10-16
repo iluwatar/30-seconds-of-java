806abd10411992fbae4246ab89d04231f67c3f12
Hammad Saeedi <faoqzuhair@gmail.com>
2023-10-16 21:50:02 +0500

    update: license

--- src/main/java/algorithm/BubbleSortSnippet.java
+++ src/main/java/algorithm/BubbleSortSnippet.java
@@ -1,13 +1,11 @@
-package algorithm;
-
 /*
- * MIT License
- *
- * Copyright (c) 2017-2022 Ilkka Seppälä
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
+* MIT License
+*
+* Copyright (c) 2017-2023 Ilkka Seppälä
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
@@ -23,6 +21,8 @@ package algorithm;
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
+package algorithm;
 
 /**
  * BubbleSortSnippet.
