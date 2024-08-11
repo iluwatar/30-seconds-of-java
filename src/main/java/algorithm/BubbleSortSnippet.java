7a47480a0e5c5089ba796058973554b62af0f5af
ishanchowdhury1-admin <thunderdizzle@gmail.com>
2024-08-11 21:56:09 +0530

    Improved Bubble Sort Algorithm

--- src/main/java/algorithm/BubbleSortSnippet.java
+++ src/main/java/algorithm/BubbleSortSnippet.java
@@ -22,29 +22,35 @@
  * SOFTWARE.
  */
 
-package algorithm;
+ package algorithm;
 
-/**
- * BubbleSortSnippet.
- */
-public class BubbleSortSnippet {
-
-  /**
-   * Sort an array with bubbleSort algorithm.
-   *
-   * @param arr array to sort
-   */
-  public static void bubbleSort(int[] arr) {
-    var lastIndex = arr.length - 1;
-
-    for (var j = 0; j < lastIndex; j++) {
-      for (var i = 0; i < lastIndex - j; i++) {
-        if (arr[i] > arr[i + 1]) {
-          var tmp = arr[i];
-          arr[i] = arr[i + 1];
-          arr[i + 1] = tmp;
-        }
-      }
-    }
-  }
-}
+ /**
+  * BubbleSortSnippet.
+  */
+ public class BubbleSortSnippet {
+ 
+   /**
+    * Sort an array using the bubble sort algorithm.
+    *
+    * @param arr array to sort
+    */
+   public static void bubbleSort(int[] arr) {
+     int lastIndex = arr.length - 1;
+     boolean sorted;
+ 
+     do {
+       sorted = true;
+       for (int i = 0; i < lastIndex; i++) {
+         if (arr[i] > arr[i + 1]) {
+           int tmp = arr[i];
+           arr[i] = arr[i + 1];
+           arr[i + 1] = tmp;
+           sorted = false; 
+         }
+       }
+ 
+       lastIndex--;
+ 
+     } while (!sorted);
+   }
+ }
