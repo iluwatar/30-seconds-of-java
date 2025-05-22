6dcce7c84ed930d1bdb62059386834496bf9a0bc
e5LA <208197507+e5LA@users.noreply.github.com>
2025-05-22 09:08:45 +0200

    refactor: fixed formatting

--- src/test/java/thread/ExecutionTimeMeasurementSnippetTest.java
+++ src/test/java/thread/ExecutionTimeMeasurementSnippetTest.java
@@ -24,9 +24,9 @@
 
 package thread;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 class ExecutionTimeMeasurementSnippetTest {
 
